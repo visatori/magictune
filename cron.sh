929e7d98d43ce2b92c50dedf396b92ca5697274c
Leonard Mocanu <leonard.mocanu@ymail.com>
2020-12-01 21:58:08 +0200

    Env vars for dry run and cron interval

--- cron.sh
+++ cron.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /app
-python magictune.py --dry-run=$DRY_RUN run > /proc/1/fd/1 2>/proc/1/fd/2
+python magictune.py --dry-run=$DRY_RUN --hide-low-volume=$HIDE_LOW_VOLUME run > /proc/1/fd/1 2>/proc/1/fd/2
